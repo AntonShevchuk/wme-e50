8409aed5be8f8401668b1c55b371d1c9be4360fc
Anton Shevchuk <antonshevchuk@gmail.com>
2022-11-20 22:14:07 +0200

    Fixed 'waiting'

--- WME-E50.user.js
+++ WME-E50.user.js
@@ -18,7 +18,7 @@
 // @grant        GM.xmlHttpRequest
 // @grant        GM.setClipboard
 // @require      https://greasyfork.org/scripts/389765-common-utils/code/CommonUtils.js?version=1090053
-// @require      https://greasyfork.org/scripts/450160-wme-bootstrap/code/WME-Bootstrap.js?version=1090054
+// @require      https://greasyfork.org/scripts/450160-wme-bootstrap/code/WME-Bootstrap.js?version=1119377
 // @require      https://greasyfork.org/scripts/452563-wme/code/WME.js?version=1101598
 // @require      https://greasyfork.org/scripts/450221-wme-base/code/WME-Base.js?version=1101617
 // @require      https://greasyfork.org/scripts/450320-wme-ui/code/WME-UI.js?version=1110180
